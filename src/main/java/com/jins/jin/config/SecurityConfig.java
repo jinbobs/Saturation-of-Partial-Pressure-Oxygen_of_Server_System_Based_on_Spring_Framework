91b8a9969bcc53e05b237451ee7a5450eb08884b
최진성 <jinbobs@github.com>
2024-10-05 23:47:21 +0900

    유저정보에 맞게 출력 성공+로그아웃기능 구현

--- src/main/java/com/jins/jin/config/SecurityConfig.java
+++ src/main/java/com/jins/jin/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.jins.jin.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,26 +13,30 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-
                 .authorizeHttpRequests((authz) -> authz
                         // 인증 없이 접근할 수 있는 경로 설정
-                        .requestMatchers( "/login", "/register", "/css/**", "/js/**").permitAll()
+                        .requestMatchers("/login", "/register", "/css/**", "/js/**").permitAll()
                         // 그 외 모든 요청은 인증 필요
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
-                        .loginPage("/login")  // 로그인 페이지 경로 설정
-                        .defaultSuccessUrl("/", true)  // 로그인 성공 후 리다이렉트할 경로
-                        .permitAll()  // 로그인 페이지는 모두에게 허용
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/", true)  // 로그인 성공 시 리다이렉트할 경로
+                        .permitAll()
                 )
-                .logout((logout) -> logout.permitAll());  // 로그아웃 허용
-
+                .logout((logout) -> logout
+                        .logoutUrl("/logout")  // 로그아웃 URL 설정
+                        .logoutSuccessUrl("/login?logout")  // 로그아웃 성공 시 리다이렉트할 경로
+                        .invalidateHttpSession(true)  // 세션 무효화
+                        .deleteCookies("JSESSIONID")  // 세션 쿠키 삭제
+                        .permitAll()
+                );
 
         return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();  // 비밀번호 암호화를 위한 BCrypt 사용
+        return new BCryptPasswordEncoder();  //  BCrypt
     }
 }
